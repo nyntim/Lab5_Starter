c6c9de3068b17bc76afde315cffb0f698e52c0a7
Tim Nguyen <tin025@ucsd.edu>
2026-05-04 21:13:41 -0700

    implement unit tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,71 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+test('isPhoneNumber: (123) 456-7890 is valid', () => {
+  expect(isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+test('isPhoneNumber: 123-456-7890 is valid', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+test('isPhoneNumber: 12345 is not valid', () => {
+  expect(isPhoneNumber('12345')).toBe(false);
+});
+test('isPhoneNumber: abc-def-ghij is not valid', () => {
+  expect(isPhoneNumber('abc-def-ghij')).toBe(false);
+});
+
+// isEmail
+test('isEmail: user@example.com is valid', () => {
+  expect(isEmail('user@example.com')).toBe(true);
+});
+test('isEmail: hello@mail.org is valid', () => {
+  expect(isEmail('hello@mail.org')).toBe(true);
+});
+test('isEmail: notanemail is not valid', () => {
+  expect(isEmail('notanemail')).toBe(false);
+});
+test('isEmail: missing@dotcom is not valid', () => {
+  expect(isEmail('missing@dotcom')).toBe(false);
+});
+
+// isStrongPassword
+test('isStrongPassword: Hello123 is valid', () => {
+  expect(isStrongPassword('Hello123')).toBe(true);
+});
+test('isStrongPassword: abcd_ is valid', () => {
+  expect(isStrongPassword('abcd_')).toBe(true);
+});
+test('isStrongPassword: 1abc is not valid (starts with number)', () => {
+  expect(isStrongPassword('1abc')).toBe(false);
+});
+test('isStrongPassword: ab is not valid (too short)', () => {
+  expect(isStrongPassword('ab')).toBe(false);
+});
+
+// isDate
+test('isDate: 12/25/2023 is valid', () => {
+  expect(isDate('12/25/2023')).toBe(true);
+});
+test('isDate: 1/1/2000 is valid', () => {
+  expect(isDate('1/1/2000')).toBe(true);
+});
+test('isDate: 2023-12-25 is not valid', () => {
+  expect(isDate('2023-12-25')).toBe(false);
+});
+test('isDate: 13/25/23 is not valid (year not 4 digits)', () => {
+  expect(isDate('13/25/23')).toBe(false);
+});
+
+// isHexColor
+test('isHexColor: #fff is valid', () => {
+  expect(isHexColor('#fff')).toBe(true);
+});
+test('isHexColor: #1a2b3c is valid', () => {
+  expect(isHexColor('#1a2b3c')).toBe(true);
+});
+test('isHexColor: #xyz is not valid', () => {
+  expect(isHexColor('#xyz')).toBe(false);
+});
+test('isHexColor: #12345 is not valid (5 digits)', () => {
+  expect(isHexColor('#12345')).toBe(false);
+});
